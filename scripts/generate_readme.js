7cb722f6f556a73ec854a74d9198eb272610dde9
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-08-18 19:21:12 +0530

    add

--- scripts/generate_readme.js
+++ scripts/generate_readme.js
@@ -7,6 +7,7 @@ const __dirname = path.dirname(__filename);
 
 const components_dir = path.resolve(__dirname, '../src/components');
 const readme_path = path.resolve(__dirname, '../README.md');
+const svgs_dir = path.resolve(__dirname, '../data/svg/');
 
 fs.readFile(readme_path, 'utf8', (err, data) => {
   if (err) {
@@ -25,14 +26,24 @@ fs.readFile(readme_path, 'utf8', (err, data) => {
   <tr>
     <th>component-name</th>
     <th>properties</th>
+    <th>preview</th>
   </tr></thead>
 <tbody>
-  ${components.map((component) => `
+  ${components.map((component) => {
+    const icon_name = component.replace(/^coreproject-/, '').replace(/^(shape-|logo-)/, '');
+    // console.log(icon_name);
+    const svg_matches = fs.readdirSync(svgs_dir).filter((svg) => svg.startsWith(icon_name));
+    // console.log(svg_matches);
+    const svg_preview = svg_matches.length > 0 ? fs.readFileSync(path.join(svgs_dir, svg_matches[0]), 'utf8') : 'no preview found';
+
+    return `
   <tr>
     <td><code>${component}</code></td>
     <td><a href='https://github.com/coreproject-moe/icons/blob/main/src/components/${component}/readme.md'>link</a></td>
+    <td>${svg_preview}</td>
   </tr>
-  `).join('')}
+  `
+  }).join('')}
 </tbody>
 </table>
 `
