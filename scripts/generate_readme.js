002cb431c06cc64c061aa11df941769952c1e238
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-08-18 20:02:18 +0530

    Revert "add"
    This reverts commit 853552d30134f69adfd28cd577d8e6293984f6f3.

--- scripts/generate_readme.js
+++ scripts/generate_readme.js
@@ -1,4 +1,4 @@
-import fs from 'node:fs';
+import fs from "node:fs";
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
@@ -10,18 +10,18 @@ const readme_path = path.resolve(__dirname, '../README.md');
 const svgs_dir = path.resolve(__dirname, '../data/svg/');
 
 fs.readFile(readme_path, 'utf8', (err, data) => {
+  if (err) {
+    return console.error("error reading readme: ", err);
+  }
+
+  fs.readdir(components_dir, { withFileTypes: true }, (err, files) => {
     if (err) {
-        return console.error('error reading readme: ', err);
+      return console.error("error reading directory: ", err);
     }
 
-    fs.readdir(components_dir, { withFileTypes: true }, (err, files) => {
-        if (err) {
-            return console.error('error reading directory: ', err);
-        }
+    const components = files.filter((dirent) => dirent.isDirectory()).map((dirent) => dirent.name);
 
-        const components = files.filter(dirent => dirent.isDirectory()).map(dirent => dirent.name);
-
-        const table_html = `
+    const table_html = `
 <table><thead>
   <tr>
     <th>component-name</th>
@@ -29,33 +29,31 @@ fs.readFile(readme_path, 'utf8', (err, data) => {
     <th></th>
   </tr></thead>
 <tbody>
-  ${components
-      .map(component => {
-          const icon_name = component.replace(/^coreproject-/, '').replace(/^(shape-|logo-)/, '');
-          // console.log(icon_name);
-          const svg_matches = fs.readdirSync(svgs_dir).filter(svg => svg.startsWith(icon_name));
-          // console.log(svg_matches);
+  ${components.map((component) => {
+    const icon_name = component.replace(/^coreproject-/, '').replace(/^(shape-|logo-)/, '');
+    // console.log(icon_name);
+    const svg_matches = fs.readdirSync(svgs_dir).filter((svg) => svg.startsWith(icon_name));
+    // console.log(svg_matches);
 
-          return `
+    return `
   <tr>
     <td><code>${component}</code></td>
     <td><img width="20" src="https://github.com/coreproject-moe/icons/blob/main/data/svg/${svg_matches[0]}" /></td>
     <td><a href='https://github.com/coreproject-moe/icons/blob/main/src/components/${component}/readme.md'>properties</a></td>
   </tr>
-  `;
-      })
-      .join('')}
+  `
+  }).join('')}
 </tbody>
 </table>
-`;
+`
 
-        const updated_data = data.replace(/## Components[\s\S]*?(?=##|$)/, `## Components\n${table_html}\n`);
+    const updated_data = data.replace(/## Components[\s\S]*?(?=##|$)/, `## Components\n${table_html}\n`);
 
-        fs.writeFile(readme_path, updated_data, 'utf8', (err, data) => {
-            if (err) {
-                return console.error('error writing readme: ', err);
-            }
-            console.log('done!');
-        });
-    });
-});
+    fs.writeFile(readme_path, updated_data, 'utf8', (err, data) => {
+      if (err) {
+        return console.error("error writing readme: ", err);
+      }
+      console.log("done!");
+    })
+  })
+})
