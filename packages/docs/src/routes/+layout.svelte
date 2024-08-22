a6a4947982b40f14e538649d42c202a5f35feac1
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-08-22 18:23:54 +0600

    Add

---
+++ packages/docs/src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { defineCustomElements } from '@coreproject-moe/icons/loader';
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		defineCustomElements();
+	});
+</script>
