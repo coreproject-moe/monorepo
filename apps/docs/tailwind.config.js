14982d2f9d6a6e0858e86eb91e177f587cbf24a4
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-08-23 10:43:50 +0530

    add kkokoro theme

--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -5,5 +5,22 @@ export default {
     extend: {},
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        kokoro: {
+          primary: "#7569E1",
+          secondary: "#03020C",
+          accent: "#FFF7F8",
+          neutral: "#1E2036",
+          "base-100": "#070519",
+          info: "#DCD9F7",
+          success: "#6FCF97",
+          warning: "#EDD68D",
+          error: "#EB5757"
+        }
+      }
+    ]
+  },
 }
 
