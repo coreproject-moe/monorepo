e71dec8c5b98a1be7c75b1f0ce073f9428d8e178
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-08-23 10:40:52 +0530

    add tailwind

--- apps/docs/src/routes/+layout.svelte
+++ apps/docs/src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import "../app.css";
+
 	import { defineCustomElements } from "@coreproject-moe/icons/loader";
 	import { onMount } from "svelte";
 	import icons from "@coreproject-moe/icons-generator";
---
+++ apps/docs/src/routes/icons/+page.svelte
@@ -0,0 +1,1 @@
+<h1 class="text-5xl font-black">Icons</h1>
