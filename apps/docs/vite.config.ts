786159d9b266e1d04dceb21d48382d65ed6c67db
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-08-23 08:02:18 +0600

    Add source map

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -2,5 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [sveltekit()],
+    build: {
+        commonjsOptions: {
+            transformMixedEsModules: true,
+        },
+        chunkSizeWarningLimit: 2048,
+        emptyOutDir: true,
+        target: 'esnext',
+        cssTarget: 'esnext',
+        minify: 'terser',
+        sourcemap: true,
+    },
 });
