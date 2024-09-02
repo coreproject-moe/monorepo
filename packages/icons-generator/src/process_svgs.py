af9be2166254b1a373d5a4080bbbbde448f9ea4b
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-09-02 09:15:42 +0530

    add

---
+++ packages/icons-generator/src/process_svgs.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+import os
+from glob import glob
+
+BASE_DIR = Path(__file__).resolve().parent
+SVGS_DIR = os.path.join(BASE_DIR, 'svg');
+
+SVG_FILES = glob(os.path.join(SVGS_DIR, '*.svg'))
+
+for svg_file in SVG_FILES:
+  with open(svg_file, 'r') as file:
+    content = file.read()
+
+  if 'white' in content:
+    updated_content = content.replace('white', 'currentColor')
+
+    with open(svg_file, 'w') as file:
+      file.write(updated_content)
+
+print('Process complete woo!');
