b6437d4c2c9f10b80ab68ea0373f0c88829058d9
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-09-03 21:51:15 +0530

    add new icons

---
+++ packages/icons/src/components/coreproject-shape-bold/coreproject-shape-bold.tsx
@@ -0,0 +1,36 @@
+import { Component, Host, h, Prop } from "@stencil/core";
+import { css_to_jsx } from "$utils/css_to_jsx";
+
+@Component({
+	tag: "coreproject-shape-bold",
+	shadow: true,
+	styleUrl: "coreproject-shape-bold.css"
+})
+export class CoreprojectShapeBold {
+	@Prop() width: string | number;
+	@Prop() height: string | number;
+	@Prop() _style: string;
+
+	render() {
+		return (
+			<Host>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="feather feather-bold"
+					height={this?.height}
+					width={this?.width}
+					style={css_to_jsx(this?._style)}
+				>
+					<path d="M6 4h8a4 4 0 0 1 4 4 4 4 0 0 1-4 4H6z"></path>
+					<path d="M6 12h9a4 4 0 0 1 4 4 4 4 0 0 1-4 4H6z"></path>
+				</svg>
+			</Host>
+		);
+	}
+}
---
+++ packages/icons/src/components/coreproject-shape-menu/coreproject-shape-menu.tsx
@@ -0,0 +1,37 @@
+import { Component, Host, h, Prop } from "@stencil/core";
+import { css_to_jsx } from "$utils/css_to_jsx";
+
+@Component({
+	tag: "coreproject-shape-menu",
+	shadow: true,
+	styleUrl: "coreproject-shape-menu.css"
+})
+export class CoreprojectShapeMenu {
+	@Prop() width: string | number;
+	@Prop() height: string | number;
+	@Prop() _style: string;
+
+	render() {
+		return (
+			<Host>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="feather feather-menu"
+					height={this?.height}
+					width={this?.width}
+					style={css_to_jsx(this?._style)}
+				>
+					<line x1="3" y1="12" x2="21" y2="12"></line>
+					<line x1="3" y1="6" x2="21" y2="6"></line>
+					<line x1="3" y1="18" x2="21" y2="18"></line>
+				</svg>
+			</Host>
+		);
+	}
+}
