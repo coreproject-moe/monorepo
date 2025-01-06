c41e89bb2bc1d584885920e3dcc0bd10abd508be
ilovemoon <moonlitgrace.gaia@gmail.com>
2025-01-06 22:58:23 +0530

    refactor: new webtorrent version and service worker (#418)

--- packages/video-player/src/components/my-component/my-component.tsx
+++ packages/video-player/src/components/my-component/my-component.tsx
@@ -1,68 +1,85 @@
 import { Component, Prop, h, State } from "@stencil/core";
-import type WebTorrent from "webtorrent";
-import "../../lib/webtorrent.min.js";
+import WebTorrent from 'webtorrent/dist/webtorrent.min.js';
 
 @Component({
-	tag: "my-component",
-	styleUrl: "my-component.css",
-	shadow: true
+  tag: "my-component",
+  styleUrl: "my-component.css",
+  shadow: true
 })
 export class MyComponent {
-	private videoElement: HTMLVideoElement | null = null;
+  private videoElement: HTMLVideoElement | null = null;
 
-	@Prop() magnet: string;
+  @Prop() magnet: string;
 
-	@State() loading: boolean = true;
-	@State() client: WebTorrent.Instance | null = null;
+  @State() loading: boolean = true;
+  @State() client: WebTorrent.Instance | null = null;
 
-	connectedCallback() {
-		if (!this.magnet) return;
-		try {
-			// console.log(this.magnet);
-			this.client = new window.WebTorrent();
-			console.log("webtorrent client initialized");
-			this.streamVideo();
-		} catch (err) {
-			console.error(err);
-		}
-	}
+  async connectedCallback() {
+    if (!this.magnet) return;
+    try {
+      // console.log(this.magnet);
+      this.client = new WebTorrent();
+      navigator.serviceWorker.register('/sw.min.js', { scope: './' }).then(reg => {
+        const worker = reg.active || reg.waiting || reg.installing
+        function checkState(worker: ServiceWorker) {
+          return worker.state === 'activated';
+        }
+        if (!checkState(worker)) {
+          worker.addEventListener('statechange', ({ target }) => {
+            const workerTarget = target as ServiceWorker;
+            if (checkState(workerTarget)) {
+              console.log('check passed', worker.state)
+              this.client.createServer({ controller: reg })
+              this.streamVideo()
+            }
+          })
+        } else {
+          console.log('check passed', worker.state)
+          this.client.createServer({ controller: reg })
+          this.streamVideo()
+        }
+      })
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
-	disconnectedCallback() {
-		if (this.client) {
-			this.client.destroy();
-			console.log("webtorrent client destroyed");
-		}
-	}
+  disconnectedCallback() {
+    if (this.client) {
+      this.client.destroy();
+      console.log("webtorrent client destroyed");
+    }
+  }
 
-	private streamVideo() {
-		if (!this.magnet || !this.client) return;
+  private streamVideo() {
+    if (!this.magnet || !this.client) return;
 
-		console.log("adding torrent with magnet:", this.magnet);
-		this.client.add(this.magnet, (torrent) => {
-			console.log("client is downloading:", torrent.infoHash);
-			const file = torrent.files.find((file) => file.name.match(/\.(mp4|mkv|webm)$/i));
-			if (file) {
-				file.renderTo(this.videoElement, () => {
-					this.loading = false; // Video is ready
-					console.log("video is streaming");
-				});
-			} else {
-				console.log("no playable video fount");
-			}
-		});
-	}
+    console.log("adding torrent with magnet:", this.magnet);
+    this.client.add(this.magnet, (torrent) => {
+      console.log("client is downloading:", torrent.infoHash);
+      const file = torrent.files.find((file) => file.name.match(/\.(mp4|mkv|webm)$/i));
+      if (file) {
+        console.log('file', file)
+        // @ts-ignore: doesnt include new version on @types/webtorrent
+        file.streamTo(this.videoElement, () => {
+          this.loading = false;
+          console.log("video is streaming");
+        });
+      } else {
+        console.log("no playable video fount");
+      }
+    });
+  }
 
-	render() {
-		return (
-			<div>
-				<video
-					ref={(el) => (this.videoElement = el)}
-					controls
-					autoPlay
-					preload="none"
-					style={{ width: "100%", height: "auto" }}
-				></video>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <video
+          ref={(el) => (this.videoElement = el)}
+          controls
+          style={{ width: "100%", height: "auto" }}
+        ></video>
+      </div>
+    );
+  }
 }
--- packages/video-player/stencil.config.ts
+++ packages/video-player/stencil.config.ts
@@ -1,26 +1,38 @@
 import { Config } from "@stencil/core";
+import copy from 'rollup-plugin-copy';
 
 export const config: Config = {
-	namespace: "video-player",
-	outputTargets: [
-		{
-			type: "dist",
-			esmLoaderPath: "../loader"
-		},
-		{
-			type: "dist-custom-elements",
-			customElementsExportBehavior: "auto-define-custom-elements",
-			externalRuntime: false
-		},
-		{
-			type: "docs-readme"
-		},
-		{
-			type: "www",
-			serviceWorker: null // disable service workers
-		}
-	],
-	testing: {
-		browserHeadless: "new"
-	}
+  namespace: "video-player",
+  outputTargets: [
+    {
+      type: "dist",
+      esmLoaderPath: "../loader"
+    },
+    {
+      type: "dist-custom-elements",
+      customElementsExportBehavior: "auto-define-custom-elements",
+      externalRuntime: false
+    },
+    {
+      type: "docs-readme"
+    },
+    {
+      type: "www",
+      serviceWorker: null // disable service workers
+    }
+  ],
+  testing: {
+    browserHeadless: "new"
+  },
+  rollupPlugins: {
+    before: [
+      copy({
+        targets: [{
+          src: '../../node_modules/webtorrent/dist/sw.min.js', dest: './www/'
+        }],
+        verbose: true,
+        copyOnce: true,
+      })
+    ]
+  }
 };
